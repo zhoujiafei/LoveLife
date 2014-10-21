26c5e8f745056660936b278ef069ac6e45625cc3
zhoujiafei <564751169@qq.com>
2014-10-21 09:02:40 +0800

    开始调整个人中心界面
    开始调整个人中心界面

---
+++ LoveLife/LoveLife/AccountHeaderTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  AccountHeaderTableViewCell.h
+//  LoveLife
+//
+//  Created by 周 加飞 on 14-10-21.
+//  Copyright (c) 2014年 com.zhouxingxing.www. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AccountHeaderTableViewCell : UITableViewCell
+
+@end
---
+++ LoveLife/LoveLife/AccountHeaderTableViewCell.m
@@ -0,0 +1,34 @@
+//
+//  AccountHeaderTableViewCell.m
+//  LoveLife
+//
+//  Created by 周 加飞 on 14-10-21.
+//  Copyright (c) 2014年 com.zhouxingxing.www. All rights reserved.
+//
+
+#import "AccountHeaderTableViewCell.h"
+
+@implementation AccountHeaderTableViewCell
+
+-(id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self)
+    {
+        
+    
+    }
+    return self;
+}
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
