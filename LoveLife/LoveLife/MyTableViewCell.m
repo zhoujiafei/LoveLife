603a9dd4c336a8062686127c236c623299c03354
zhoujiafei <564751169@qq.com>
2014-10-23 12:53:19 +0800

    添加自定义button
    添加自定义button

--- LoveLife/LoveLife/MyTableViewCell.m
+++ LoveLife/LoveLife/MyTableViewCell.m
@@ -23,6 +23,9 @@
         _cellTitle = [[UILabel alloc] initWithFrame:CGRectMake(60, 7, 100, 30)];
         _cellTitle.font = [UIFont systemFontOfSize:16];
         
+        UIImageView *rightArrow = [[UIImageView alloc] initWithFrame:CGRectMake(290, 17, 10, 13.5)];
+        rightArrow.image = [UIImage imageNamed:@"userInfo_arrow"];
+        
         //cell头部的线
         UIView *topLine = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, 0.5)];
         topLine.backgroundColor = [UIColor colorWithRed:213.0f/255.0f green:213.0f/255.0f blue:213.0f/255.0f alpha:1.0];
@@ -32,6 +35,7 @@
         bottomLine.backgroundColor = [UIColor colorWithRed:213.0f/255.0f green:213.0f/255.0f blue:213.0f/255.0f alpha:1.0];
 
         [self addSubview:_imageView];
+        [self addSubview:rightArrow];
         [self addSubview:_cellTitle];
         [self addSubview:bottomLine];
         [self addSubview:topLine];
---
+++ LoveLife/LoveLife/UserCenterButton.h
@@ -0,0 +1,13 @@
+//
+//  UserCenterButton.h
+//  LoveLife
+//
+//  Created by 周 加飞 on 14-10-23.
+//  Copyright (c) 2014年 com.zhouxingxing.www. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserCenterButton : UIButton
+
+@end
